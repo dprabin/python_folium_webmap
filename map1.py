5500ba2413a7f3715fc465515f8d2e81028d8c5f
Prabin Babu Dhakal <dprabin@yahoo.com>
2018-12-20 02:14:21 +0545

    Added the marker with feature map so that you can add multiple features at once

--- map1.py
+++ map1.py
@@ -1,4 +1,9 @@
 import folium
 map=folium.Map(location=[27.7,85.35],zoom_start=7, tiles="Mapbox Bright")
+
+
+fg=folium.FeatureGroup(name="My Map")
 map.add_child(folium.Marker(location=[27.8,85.0], popup="This is new marker",icon=folium.Icon(color="green")))
+map.add_child(fg)
+
 map.save("Map1.html")
