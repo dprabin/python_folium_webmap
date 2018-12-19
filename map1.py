24d0a481483125ea902e84784cec5dbfae4ca9e9
Prabin Babu Dhakal <dprabin@yahoo.com>
2018-12-20 02:08:49 +0545

    Added icon to the marker

--- map1.py
+++ map1.py
@@ -1,4 +1,4 @@
 import folium
 map=folium.Map(location=[27.7,85.35],zoom_start=7, tiles="Mapbox Bright")
-map.add_child(folium.Marker(location=[27.66,85.33], popup="This is new marker"))
+map.add_child(folium.Marker(location=[27.8,85.0], popup="This is new marker",icon=folium.Icon(color="green")))
 map.save("Map1.html")
