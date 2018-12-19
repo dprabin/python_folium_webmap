eca5780fde7cbaaa07caa9b8bb16ec756655a8cd
Prabin Babu Dhakal <dprabin@yahoo.com>
2018-12-20 02:04:11 +0545

    Added marker and a popup for that marker

--- map1.py
+++ map1.py
@@ -1,3 +1,4 @@
 import folium
 map=folium.Map(location=[27.7,85.35],zoom_start=7, tiles="Mapbox Bright")
+map.add_child(folium.Marker(location=[27.66,85.33], popup="This is new marker"))
 map.save("Map1.html")
