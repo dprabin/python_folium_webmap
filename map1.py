3147ed7d4a6df33eefecdecdb170e615a8095e59
Prabin Babu Dhakal <dprabin@yahoo.com>
2018-12-20 10:59:09 +0545

    iterate through latitudes and longitudes to add folium icon at different places

--- map1.py
+++ map1.py
@@ -1,9 +1,17 @@
 import folium
-map=folium.Map(location=[27.7,85.35],zoom_start=7, tiles="Mapbox Bright")
+import pandas
 
+data = pandas.read_csv("Volcanoes_USA.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+#map=folium.Map(location=[27.7,85.35],zoom_start=7, tiles="Mapbox Bright")
+map=folium.Map(location=[38.58,-99.09],zoom_start=6, tiles="Mapbox Bright")
 
 fg=folium.FeatureGroup(name="My Map")
-map.add_child(folium.Marker(location=[27.8,85.0], popup="This is new marker",icon=folium.Icon(color="green")))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" meters",icon=folium.Icon(color="green")))
 map.add_child(fg)
 
 map.save("Map1.html")
