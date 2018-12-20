c0e7a799c1c8c9722f76eecc253f97bf2928da25
Prabin Babu Dhakal <dprabin@yahoo.com>
2018-12-20 11:08:44 +0545

    change color of icon according to height of volcanic mountain

--- map1.py
+++ map1.py
@@ -6,12 +6,20 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
+def get_color(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 #map=folium.Map(location=[27.7,85.35],zoom_start=7, tiles="Mapbox Bright")
-map=folium.Map(location=[38.58,-99.09],zoom_start=6, tiles="Mapbox Bright")
+map=folium.Map(location=[38.58,-99.09],zoom_start=5, tiles="Mapbox Bright")
 
 fg=folium.FeatureGroup(name="My Map")
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" meters",icon=folium.Icon(color="green")))
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" meters",icon=folium.Icon(color=get_color(el))))
 map.add_child(fg)
 
 map.save("Map1.html")
