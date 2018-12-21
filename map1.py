4244789130790efac280c1bdd5e74d747375168e
Prabin Babu Dhakal <dprabin@yahoo.com>
2018-12-21 10:47:32 +0545

    added folium GeoJson data for all countries

--- map1.py
+++ map1.py
@@ -20,6 +20,9 @@ map=folium.Map(location=[38.58,-99.09],zoom_start=5, tiles="Mapbox Bright")
 fg=folium.FeatureGroup(name="My Map")
 for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.CircleMarker(location=[lt, ln], popup=str(el)+" meters",fill_color=(get_color(el)),color='grey',fill_opacity=0.5))
+
+fg.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig').read()))
+
 map.add_child(fg)
 
 map.save("Map1.html")
