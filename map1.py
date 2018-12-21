8c69f8d99545b1766b38efbab4da876213ce9748
Prabin Babu Dhakal <dprabin@yahoo.com>
2018-12-21 10:28:44 +0545

    Changed marker shape and color

--- map1.py
+++ map1.py
@@ -19,7 +19,7 @@ map=folium.Map(location=[38.58,-99.09],zoom_start=5, tiles="Mapbox Bright")
 
 fg=folium.FeatureGroup(name="My Map")
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" meters",icon=folium.Icon(color=get_color(el))))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], popup=str(el)+" meters",fill_color=(get_color(el)),color='grey',fill_opacity=0.5))
 map.add_child(fg)
 
 map.save("Map1.html")
